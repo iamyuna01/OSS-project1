0758ef18305a7e9202ec7ec79466b051e6e3c468
iamyuna01 <iamu.design01@gmail.com>
2024-11-13 14:31:37 +0900

    Test 브랜치에 코드를 추가함

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -2,8 +2,14 @@
 
 int main()
 {
+    int i = 9;
+
     printf("First OSS Code\n");
     printf("Second OSS Code\n");
 
+    for (int i = 0; i < 10; i++){
+        printf("Test 브랜칰\n");
+    }
+    
     return 0;
 }
